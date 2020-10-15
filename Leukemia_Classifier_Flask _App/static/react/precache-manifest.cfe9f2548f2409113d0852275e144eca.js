f2a314cfa05f022e883be8d3d1e3de92fb892ddc
Matthew Gibson <mattgibson6@gmail.com>
2020-10-15 17:28:20 -0600

    initialize react fe

---
+++ Leukemia_Classifier_Flask _App/static/react/precache-manifest.cfe9f2548f2409113d0852275e144eca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a897c091dc4eb9051fb3ceaf1ad521",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "16249c425dd6f712a0cd",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fcdea881f01fc51e13d7",
+    "url": "/static/react/js/2.eeccef0c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/react/js/2.eeccef0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16249c425dd6f712a0cd",
+    "url": "/static/react/js/main.9c60f82e.chunk.js"
+  },
+  {
+    "revision": "2178e029b8451cecdc2a",
+    "url": "/static/react/js/runtime-main.514c39d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
